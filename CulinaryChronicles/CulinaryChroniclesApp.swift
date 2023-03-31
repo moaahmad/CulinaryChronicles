a96dec3ca824c163ee242cd71e2065eea10f43f0
Mo Ahmad <moaahmad@gmail.com>
2023-03-31 15:24:16 +0100

    Initial Commit

---
+++ CulinaryChronicles/CulinaryChroniclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulinaryChroniclesApp.swift
+//  CulinaryChronicles
+//
+//  Created by Mo Ahmad on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CulinaryChroniclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
